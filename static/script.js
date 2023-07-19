daab632f69e7fbbc0a0764a25b292e428a3bbc42
Austin Dreosch <austindreosch@gmail.com>
2023-07-18 20:41:48 -0700

    localizing to deployment server

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 
 let lastClickedID = 0;
-let serverBaseURL = "http://127.0.0.1:5000";
+let serverBaseURL = "https://prepkitchen.onrender.com";
 
 $(document).ready(function() {
     console.log( "ready!" );
