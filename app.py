d0e601a95fa88de1d2c1f53809c639e62da7c250
Austin Dreosch <austindreosch@gmail.com>
2023-07-19 19:23:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,12 +60,13 @@ def choose_plan(plan_id):
 
     if 'cart_array' in session:
         # turn session string into a list of ids
-        session_cart = session.get('cart_array', [])
+        session_cart = session['cart_array'].lstrip(
+            '["').rstrip('"]').split('","')
         id_cart = [eval(i) for i in session_cart]
         cart_length = len(id_cart)
         # reset cart if too many items for new plan
         if plan.meal_count < cart_length:
-            session['cart_array'] = []
+            del session['cart_array']
             session['cart_length'] = 0
 
     return redirect('/choose/pork')
